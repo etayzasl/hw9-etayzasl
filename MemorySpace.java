56f4786aa9b39e03eaa9798511c69bff4c3213d4
Etay <105246259+EtayZaslavsky@users.noreply.github.com>
2025-01-23 22:44:45 +0200

    second version LinkkedList

--- MemorySpace.java
+++ MemorySpace.java
@@ -30,48 +30,55 @@ public class MemorySpace {
 	/**
 	 * Allocates a memory block of a requested length (in words). Returns the
 	 * base address of the allocated block, or -1 if unable to allocate.
-	 * 
-	 * This implementation scans the freeList, looking for the first free memory block 
-	 * whose length equals at least the given length. If such a block is found, the method 
-	 * performs the following operations:
-	 * 
-	 * (1) A new memory block is constructed. The base address of the new block is set to
-	 * the base address of the found free block. The length of the new block is set to the value 
-	 * of the method's length parameter.
-	 * 
-	 * (2) The new memory block is appended to the end of the allocatedList.
-	 * 
-	 * (3) The base address and the length of the found free block are updated, to reflect the allocation.
-	 * For example, suppose that the requested block length is 17, and suppose that the base
-	 * address and length of the the found free block are 250 and 20, respectively.
-	 * In such a case, the base address and length of of the allocated block
-	 * are set to 250 and 17, respectively, and the base address and length
-	 * of the found free block are set to 267 and 3, respectively.
-	 * 
-	 * (4) The new memory block is returned.
-	 * 
-	 * If the length of the found block is exactly the same as the requested length, 
-	 * then the found block is removed from the freeList and appended to the allocatedList.
-	 * 
-	 * @param length
-	 *        the length (in words) of the memory block that has to be allocated
-	 * @return the base address of the allocated block, or -1 if unable to allocate
 	 */
-	public int malloc(int length) {		
-		//// Replace the following statement with your code
-		return -1;
+	public int malloc(int length) {
+		if (length <= 0) {
+			return -1;
+		}
+
+		ListIterator iterator = freeList.iterator();
+		while (iterator.hasNext()) {
+			MemoryBlock freeBlock = iterator.next();
+			if (freeBlock.length >= length) {
+				int baseAddress = freeBlock.baseAddress;
+				
+				// Create new allocated block
+				MemoryBlock allocatedBlock = new MemoryBlock(baseAddress, length);
+				allocatedList.addLast(allocatedBlock);
+				
+				if (freeBlock.length == length) {
+					// If exact size match, remove the free block
+					freeList.remove(freeBlock);
+				} else {
+					// If larger, update the free block
+					freeBlock.baseAddress = baseAddress + length;
+					freeBlock.length = freeBlock.length - length;
+				}
+				
+				return baseAddress;
+			}
+		}
+		return -1;  // Unable to allocate
 	}
 
 	/**
 	 * Frees the memory block whose base address equals the given address.
-	 * This implementation deletes the block whose base address equals the given 
-	 * address from the allocatedList, and adds it at the end of the free list. 
-	 * 
-	 * @param baseAddress
-	 *            the starting address of the block to freeList
 	 */
 	public void free(int address) {
-		//// Write your code here
+		if (allocatedList.getSize() == 0) {
+			throw new IllegalArgumentException("index must be between 0 and size");
+		}
+		
+		ListIterator iterator = allocatedList.iterator();
+		while (iterator.hasNext()) {
+			MemoryBlock block = iterator.next();
+			if (block.baseAddress == address) {
+				allocatedList.remove(block);
+				freeList.addLast(block);
+				return;
+			}
+		}
+		throw new IllegalArgumentException("Address not found in allocated blocks");
 	}
 	
 	/**
@@ -79,16 +86,56 @@ public class MemorySpace {
 	 * for debugging purposes.
 	 */
 	public String toString() {
-		return freeList.toString() + "\n" + allocatedList.toString();		
+		return freeList.toString() + " \n" + allocatedList.toString() + " ";
 	}
 	
 	/**
-	 * Performs defragmantation of this memory space.
-	 * Normally, called by malloc, when it fails to find a memory block of the requested size.
-	 * In this implementation Malloc does not call defrag.
+	 * Performs defragmentation of this memory space.
 	 */
 	public void defrag() {
-		/// TODO: Implement defrag test
-		//// Write your code here
+		if (freeList.getSize() <= 1) return;
+		
+		// Sort free blocks by base address
+		// We'll do this by creating a new list and inserting in order
+		LinkedList sortedList = new LinkedList();
+		ListIterator iterator = freeList.iterator();
+		
+		while (iterator.hasNext()) {
+			MemoryBlock block = iterator.next();
+			
+			// Find the right position to insert
+			int index = 0;
+			ListIterator sortedIterator = sortedList.iterator();
+			while (sortedIterator.hasNext()) {
+				MemoryBlock sortedBlock = sortedIterator.next();
+				if (block.baseAddress < sortedBlock.baseAddress) {
+					break;
+				}
+				index++;
+			}
+			sortedList.add(index, block);
+		}
+		
+		// Replace freeList with sorted list
+		freeList = sortedList;
+		
+		// Now merge adjacent blocks
+		iterator = freeList.iterator();
+		MemoryBlock prevBlock = iterator.next();
+		
+		while (iterator.hasNext()) {
+			MemoryBlock currentBlock = iterator.next();
+			if (prevBlock.baseAddress + prevBlock.length == currentBlock.baseAddress) {
+				// Merge blocks
+				prevBlock.length = prevBlock.length + currentBlock.length;
+				freeList.remove(currentBlock);
+				// Start over since we modified the list
+				iterator = freeList.iterator();
+				prevBlock = iterator.next();
+			} else {
+				prevBlock = currentBlock;
+			}
+		}
 	}
 }
+
